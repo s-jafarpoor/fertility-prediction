8270fdc859cf8662aa99930d658700aad6e7b2bb
s-jafarpoor <shabnamjafarpoor1371@gmail.com>
2025-03-04 09:47:54 +0330

    Update server.py

--- server.py
+++ server.py
@@ -196,3 +196,7 @@ async def predict(data: InputData):
     except Exception as e:
         return {"error": str(e)}
 
+if __name__ == "__main__":
+    import uvicorn
+    port = int(os.environ.get("PORT", 10000))  # مقدار پورت را از متغیر محیطی می‌گیرد
+    uvicorn.run(app, host="0.0.0.0", port=port)
